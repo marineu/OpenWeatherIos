0fe6356f823a6ddecfb7ae1b19f8f11a463a5180
Marin Ipati <ipatimarin@gmail.com>
2021-05-02 22:36:56 +0100

    Added Constants and  CurrentWeatherController classes

---
+++ OpenWeatherIos/Modules/Common/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  OpenWeatherIos
+//
+//  Created by Marin Ipati on 02/05/2021.
+//
+
+import Foundation
+
+class Constants {
+    
+    static var apiKey = "7140d6d1b230840f1dc272efb0001e86"
+    
+}
---
+++ OpenWeatherIos/Modules/CurrentWeather/ViewControllers/CurrentWeatherController.swift
@@ -0,0 +1,23 @@
+//
+//  CurrentWeatherController.swift
+//  OpenWeatherIos
+//
+//  Created by Marin Ipati on 02/05/2021.
+//
+
+import UIKit
+
+class CurrentWeatherController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        
+        self.view = view
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
