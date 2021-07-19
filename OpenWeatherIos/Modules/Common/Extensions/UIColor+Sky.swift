957485bb7465e8393ed961ec858afa0ef2023711
Marin Ipati <ipatimarin@gmail.com>
2021-07-19 17:01:14 +0300

    Added sky colors

---
+++ OpenWeatherIos/Modules/Common/Extensions/UIColor+Sky.swift
@@ -0,0 +1,75 @@
+//
+//  UIColor+Sky.swift
+//  OpenWeatherIos
+//
+//  Created by Marin Ipati on 19/07/2021.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static var clearNightStart: UIColor {
+        return UIColor(named: "clear-night-start") ?? .clear
+    }
+
+    static var clearNightEnd: UIColor {
+        return UIColor(named: "clear-night-end") ?? .clear
+    }
+
+    static var cloudyFoggyNightStart: UIColor {
+        return UIColor(named: "cloudy-foggy-night-start") ?? .clear
+    }
+
+    static var cloudyFoggyNightEnd: UIColor {
+        return UIColor(named: "cloudy-foggy-night-end") ?? .clear
+    }
+
+    static var rainyNightStart: UIColor {
+        return UIColor(named: "rainy-night-start") ?? .clear
+    }
+
+    static var rainyNightEnd: UIColor {
+        return UIColor(named: "rainy-night-end") ?? .clear
+    }
+
+    static var snowyIcyNightStart: UIColor {
+        return UIColor(named: "snowy-icy-night-start") ?? .clear
+    }
+
+    static var snowyIcyNightEnd: UIColor {
+        return UIColor(named: "snowy-icy-night-end") ?? .clear
+    }
+
+    static var sunnyDayStart: UIColor {
+        return UIColor(named: "sunny-day-start") ?? .clear
+    }
+
+    static var sunnyDayEnd: UIColor {
+        return UIColor(named: "sunny-day-end") ?? .clear
+    }
+
+    static var cloudyFoggyDayStart: UIColor {
+        return UIColor(named: "cloudy-foggy-day-start") ?? .clear
+    }
+
+    static var cloudyFoggyDayEnd: UIColor {
+        return UIColor(named: "cloudy-foggy-day-end") ?? .clear
+    }
+
+    static var rainyDayStart: UIColor {
+        return UIColor(named: "rainy-day-start") ?? .clear
+    }
+
+    static var rainyDayEnd: UIColor {
+        return UIColor(named: "rainy-day-end") ?? .clear
+    }
+
+    static var snowyIcyDayStart: UIColor {
+        return UIColor(named: "snowy-icy-day-start") ?? .clear
+    }
+
+    static var snowyIcyDayEnd: UIColor {
+        return UIColor(named: "snowy-icy-day-end") ?? .clear
+    }
+}
